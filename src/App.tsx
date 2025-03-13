53be5407ae6f8f60aea62c2b16b207e5d30c4c4e
Fr4nk139 <francesco.fabiano06@icloud.com>
2025-03-13 14:52:13 +0000

    tipi e layout

--- src/App.tsx
+++ src/App.tsx
@@ -65,20 +65,105 @@ const data = [
     image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/9.png",
     types: ["water"]
   },
-]
-export const App = () => {
-  return <div >
-    <div  className="flex flex-row">
-      {data.map((item) =>{
-        return (
-          <div>
-            {item.id} - {item.name}
-            <img src = {item.image}/>
+  {
+    id: 10, 
+    name: "Caterpie",
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/10.png",
+    types: ["bug"]
+  },
+  {
+    id: 11, 
+    name: "Metapod",
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/11.png",
+    types: ["bug"]
+  },
+  {
+    id: 12, 
+    name: "Butterfree",
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/12.png",
+    types: ["bug", "flying"]
+  },
+  {
+    id: 13, 
+    name: "Weedle",
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/13.png",
+    types: ["bug", "poison"]
+  },
+  {
+    id: 14, 
+    name: "Kakuna",
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/14.png",
+    types: ["bug", "poison"]
+  },
+  {
+    id: 15, 
+    name: "Beedrill",
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/15.png",
+    types: ["bug", "poison"]
+  },
+  {
+    id: 16, 
+    name: "Pidgey",
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/16.png",
+    types: ["normal", "flying"]
+  },
+  {
+    id: 17, 
+    name: "Pidgeotto",
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/17.png",
+    types: ["normal", "flying"]
+  },
 
-            
-      
+];
+const typeColors: {[key: string]: string} = {
+  fire: "bg-red-500",
+  water: "bg-blue-500",
+  poison: "bg-purple-600",
+  flying: "bg-sky-300",
+  grass: "bg-green-500",
+  bug: "bg-lime-500",
+  normal: "bg-gray-400",
+  fighting: "bg-amber-800",
+  eletric: "bg-yellow-300",
+  ground: "bg-yellow-900",
+  psychic: "bg-fuchsia-400",
+  rock: "bg-grey-500",
+  ice: "bg-blue-300",
+  dragon: "bg-teal-400",
+  ghost: "bg-purple-400",
+};
+function getColoreTipo(type: string){
+  return typeColors[type];
+}
+interface CardProps{
+  id: number;
+  image: string;
+  name: string;
+  types: string[];
+}
+const Card = (props: CardProps) => {
+  return (
+    <div className="bg-white w-2xs">
+      {props.id} - {props.name}
+      <img src = {props.image}/>
+      <div className="flex flex-row gap-4">
+      {props.types.map((type) => {
+        return (
+          <div className={`p-3 mx-3 ${getColoreTipo(type)}`}>
+            {type}
           </div>
         )
+      })}
+      </div>
+    </div>
+  )
+}
+
+export const App = () => {
+  return <div >
+    <div  className="flex flex-wrap gap-4 p-4">
+      {data.map((item) =>{
+          return <Card  id={item.id} name={item.name} image={item.image} types = {item.types}/>
       }
     )}
     </div>
